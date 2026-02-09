aeb4778fe80a9ed5847b0981fd0493d6133e87e8
hihong <146538401+hihong1023@users.noreply.github.com>
2026-02-09 14:27:24 +0800

    Update telegram.py

--- app/telegram.py
+++ app/telegram.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-TELEGRAM_BOT_TOKEN = os.getenv("8438811074:AAFR-FAMKmSEPU3QQ8S-NBcgcx2hgerZdUs")
-TELEGRAM_CHAT_ID = os.getenv("-5145199200")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram_message(text: str):
     if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
@@ -11,9 +11,8 @@ def send_telegram_message(text: str):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
 
     payload = {
-        "chat_id": int(TELEGRAM_CHAT_ID),
+        "chat_id": TELEGRAM_CHAT_ID,   # keep as STRING
         "text": text,
-        "parse_mode": "Markdown"
     }
 
     r = requests.post(url, json=payload, timeout=5)
